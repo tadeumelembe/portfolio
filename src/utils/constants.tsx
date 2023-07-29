6bdfab7a72c0898b738b030def4508ef3601a4e3
tadeumelembe <tadeumelembe@gmail.com>
2023-07-30 00:07:23 +0200

    Added ser data into a variable

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,2 +1,30 @@
 export const responsivePadding = 'px-10 xl:px-20 [1330px]:px-0'
 export const maxWidthContent = 'max-w-7xl mx-auto'
+export const userData = {
+    "first_name": 'Tadeu',
+    "last_name": "Melembe",
+    "small_bio": "I'm a Software Engineer focused on building mobile and web solutions that solve real life problems.",
+    "bio": `Sometimes I feel like a "Super Heroe", and my skills are my "Super Power". I can use them to create solutions that can impact society and change people's life, making it better and easier.`,
+    "experiemce": [
+        {
+            "company": "Freelance",
+            "position": "Mobile Developement",
+            "start_date": "2022",
+            "end_date": "2022",
+            "job_descriotion": ""
+        },
+        {
+            "company": "Paytech, SA",
+            "position": "Full-Stack Web Developer",
+            "start_date": "2020",
+            "end_date": "2021",
+            "job_descriotion": ""
+        }
+    ],
+    "projects": [
+        {
+            "name": "Recipe-Reels",
+            "description": ""
+        }
+    ]
+}
