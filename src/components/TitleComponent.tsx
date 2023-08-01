b381d0c35bd558ef0ff43b1a901e9a9001e63a2a
tadeumelembe <tadeumelembe@gmail.com>
2023-08-01 03:17:55 +0200

    Added motionnto title comppnent

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -1,5 +1,6 @@
 'use client'
 import localFont from "next/font/local";
+import { motion } from "framer-motion";
 
 const tektur = localFont({ src: '../../public/fonts/Tektur-VariableFont_wdth,wght.ttf' })
 
@@ -9,12 +10,28 @@ type TitleT = {
 export default function Title(props: TitleT) {
     return (
         <div className="sm:flex flex-row justify-between gap-10 mt-40 items-center">
-            <div className="sm:w-1/2">
+            <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 0.7,
+                    delay: 0.5
+                }}
+                className="sm:w-1/2">
                 <h2 className={`${tektur.className} font-bold text-3xl`}>{props.title}</h2>
-            </div>
-            <div className="sm:w-1/2 mt-5 sm:mt-0">
+            </motion.div>
+
+            <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 0.7,
+                    delay: 0.5
+                }} className="sm:w-1/2 mt-5 sm:mt-0">
                 <div className=" border-t-4 border-green-water w-full" />
-            </div>
+            </motion.div>
 
         </div>
 
