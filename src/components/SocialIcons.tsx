e65b9a910139eb80ce3c2169f0dd2fedd2b6e14d
Tadeu Melembe <tadeumelembe@gmail.com>
2026-03-03 21:24:38 +0200

    refactor: remove social icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,8 +5,8 @@ export default function SocialIcons() {
         <>
             <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://github.com/tadeumelembe" />
             <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://www.linkedin.com/in/tadeumelembe" />
-            <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://www.instagram.com/code._daily/" />
-            <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://www.twitter.com/tadeumelembe/" />
+            {/* <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://www.instagram.com/code._daily/" /> */}
+            {/* <SocialIcon bgColor="#708DB600" fgColor="#708DB6" target="_blank" url="https://www.twitter.com/tadeumelembe/" /> */}
         </>
     )
 }
